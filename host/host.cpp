61b086788418e9277a3529fa4c0ae3b088bb995c
Rene Hopf <renehopf@mac.com>
2012-11-11 19:40:31 +0100

    bulk transfer working

--- host/host.cpp
+++ host/host.cpp
@@ -11,7 +11,7 @@ int main() {
 	int r; //for return values
 	ssize_t cnt; //holding number of devices in list
   	unsigned char data[64];
-	int i;
+	int j;
 
   	for(int j=0;j<64;j++) { data[j] = 0; }
     data[0] = 0;
@@ -24,19 +24,22 @@ int main() {
 	//libusb_set_debug(ctx, 3); //set verbosity level to 3, as suggested in the documentation
 
 	dev = libusb_open_device_with_vid_pid(ctx, 0xcafe, 0xcafe);
+	libusb_set_configuration(dev,0);
   	libusb_claim_interface(dev, 0);
 
 /**/
-	time_t t = time(0);
-  for(int i = 0; i < 20000; i++){
-	libusb_control_transfer(dev, 0x40, 0x00, 0, 0, data, sizeof(data), 0);
-    //libusb_bulk_transfer(dev, 0x82, data, sizeof(data), &i, 0);
+  float times = 10000;
+  time_t t = time(0);
+  for(int i = 0; i < times; i++){
+	//libusb_control_transfer(dev, 0x40, 0x00, 0, 0, data, sizeof(data), 0);
+    libusb_bulk_transfer(dev, 0x01, data, sizeof(data), &j, 0);
+    //cout << "j:" << j << endl;
     //libusb_iso_transfer(dev, 0x83, data, sizeof(data), &i, 0);
     data[0] = 255 - data[0];
   }
   time_t t2 = time(0);
-    cout << (float)(t2 - t) / 1 << "s" << endl << flush;
-  cout << (float)(t2 - t) / 1 / 20000.0 << "s / transfer" << endl << flush;
+  cout << (float)(t2 - t) << "s" << endl << flush;
+  cout << (float)((t2 - t) / times) << "s / transfer" << endl << flush;
 
 
 
--- stm-hal.c
+++ stm-hal.c
@@ -18,17 +18,29 @@ const struct usb_device_descriptor dev = {
 	.iSerialNumber = 3,
 	.bNumConfigurations = 1,
 };
-
+//
+static const struct usb_endpoint_descriptor data_endp[] = {
+    {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x01,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+	.bInterval = 1,
+    }
+};
+//
 const struct usb_interface_descriptor iface = {
 	.bLength = USB_DT_INTERFACE_SIZE,
 	.bDescriptorType = USB_DT_INTERFACE,
 	.bInterfaceNumber = 0,
 	.bAlternateSetting = 0,
-	.bNumEndpoints = 0,
+	.bNumEndpoints = 1,
 	.bInterfaceClass = 0xFF,
 	.bInterfaceSubClass = 0,
 	.bInterfaceProtocol = 0,
 	.iInterface = 0,
+	.endpoint = data_endp,
 };
 
 const struct usb_interface ifaces[] = {{
@@ -74,12 +86,39 @@ static int simple_control_callback(usbd_device *usbd_dev, struct usb_setup_data 
 	
 	
 
-    if(buf[0][0] == 0x00)
-        gpio_set(GPIOD, GPIO12);
-    if(buf[0][0] == 0xff)
-        gpio_clear(GPIOD, GPIO12);
+    //if(buf[0][0] == 0x00)
+    //    gpio_set(GPIOD, GPIO12);
+    //if(buf[0][0] == 0xff)
+    //    gpio_clear(GPIOD, GPIO12);
 
 	return 1;
+}
+
+static void cdcacm_data_rx_cb(usbd_device *usbd_dev, u8 ep)
+{
+	(void)ep;
+
+	char buf[64];
+	int len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
+
+	//if (len) {
+	//	while (usbd_ep_write_packet(usbd_dev, 0x82, buf, len) == 0)
+	//		;
+	//}
+
+	gpio_toggle(GPIOD, GPIO12);
+}
+
+static void cdcacm_set_config(usbd_device *usbd_dev, u16 wValue)
+{
+	(void)wValue;
+	gpio_toggle(GPIOD, GPIO13);
+	usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64, cdcacm_data_rx_cb);		
+	usbd_register_control_callback(
+				usbd_dev,
+				USB_REQ_TYPE_VENDOR,
+				USB_REQ_TYPE_TYPE,
+				simple_control_callback);
 }
 
 int main(void)
@@ -99,13 +138,9 @@ int main(void)
 	/* LED output */
 	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
 
-	usbd_dev = usbd_init(&stm32f107_usb_driver, &dev, &config, usb_strings);
-	usbd_register_control_callback(
-				usbd_dev,
-				USB_REQ_TYPE_VENDOR,
-				USB_REQ_TYPE_TYPE,
-				simple_control_callback);
-
+	usbd_dev = usbd_init(&otgfs_usb_driver, &dev, &config, usb_strings);
+	usbd_register_set_config_callback(usbd_dev, cdcacm_set_config);
+	
 	while (1)
 		usbd_poll(usbd_dev);
 }
